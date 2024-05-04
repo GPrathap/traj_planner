583f109178acbdbd33ed47cbff8b5f9d18b764b9
GPrathap <ggeesara@gmail.com>
2024-05-04 15:13:22 +0100

    adding force mag

--- generate_force_profile.py
+++ generate_force_profile.py
@@ -12,16 +12,21 @@ class GenerateForceProfile:
     def add_noise_to_normal_vectors(self, N, noise_level=0.01):
         noise = noise_level * (np.random.rand(N.shape[0], N.shape[1]) - 0.5)
         return N + noise
-
-    def plot_trajectory_and_curvature(self, x, y, z, T, N, B, kappa, tau, kappa_min_max
-                                    , tau_min_max, view, ax=None, color="b", length=0.1):
+    
+    def get_magnitude(self, func, a, b, points):
+        mag_values = np.linspace(a, b, points)
+        return func(mag_values)
+    
+    
+    def plot_trajectory_and_curvature(self, x, y, z, N, kappa, ax=None, color="b"):
         scale_factor = 0.1
+        magnitudes = np.linalg.norm(N, axis=1)
         for i in range(len(x)):
             end_point = np.array([x[i], y[i], z[i]])
             curvature_direction = scale_factor * kappa[i] * N[i]
             ax.quiver(end_point[0], end_point[1], end_point[2],
                     curvature_direction[0], curvature_direction[1], curvature_direction[2]
-                                    , color=color, length=length, normalize=True)
+                                    , color=color, length=magnitudes[i], normalize=True)
 
     def getRefinedTrajecotry(self, example_id=1, noise_level=0.21, k=15):
         if example_id == 0 or example_id is None:
@@ -51,28 +56,23 @@ class GenerateForceProfile:
         dB = np.gradient(B, axis=0)
         tau = -np.sum(dB * N, axis=1) / ds
 
-        # fig, axes = plt.subplots(1, 2, figsize=(16, 8), subplot_kw={'projection': '3d'})
-        # ax = fig.add_subplot(111, projection='3d')
         fig = plt.figure(figsize=(10, 8))
         ax = fig.add_subplot(111, projection='3d')
-        force_profile = N
-        self.plot_trajectory_and_curvature(x, y, z, T, force_profile, B, kappa, tau, kappa_min_max
-                                    , tau_min_max, view, ax=ax, color="r", length=0.1)
-
-        force_profile_with_noise = self.add_noise_to_normal_vectors(N, noise_level=noise_level)
-        self.plot_trajectory_and_curvature(x, y, z, T, force_profile_with_noise
-                                    , B, kappa, tau, kappa_min_max, tau_min_max, view, ax=ax
-                                    , color="b", length=0.1)
+        force_mag = self.get_magnitude(np.sin, -0.2, 0.2, len(x))
+        
+        force_profile = (N.T*force_mag).T
+        self.plot_trajectory_and_curvature(x, y, z, force_profile, kappa, ax=ax, color=(1, 0.0, 0.0, 0.1))
+        
+        force_profile_with_noise = self.add_noise_to_normal_vectors(force_profile, noise_level=noise_level)
+        self.plot_trajectory_and_curvature(x, y, z, force_profile_with_noise, kappa, ax=ax, color=(0.0, 1.0, 0.0, 0.1))
         
         resultant_foruce = force_profile + force_profile_with_noise
-        self.plot_trajectory_and_curvature(x, y, z, T, resultant_foruce
-                                    , B, kappa, tau, kappa_min_max, tau_min_max, view, ax=ax
-                                    , color="g", length=0.1)
+        self.plot_trajectory_and_curvature(x, y, z, resultant_foruce, kappa, ax=ax, color=(0.0, 0.0, 1.0, 0.1))
         
         sub_traj = resultant_foruce/k
-        
         ax.plot(x, y, z, label='initial trajecotry', color="k")
-        ax.plot(x + sub_traj[:,0], y + sub_traj[:,1], z + sub_traj[:,2], label='refined trajecotry', color="y")
+        ax.plot(x + sub_traj[:,0], y + sub_traj[:,1], z + sub_traj[:,2]
+                , label='refined trajecotry', color="y", linewidth=3.0)
         
         ax.set_title(f'With Noise (level={noise_level})')
         ax.set_title('Trajectory and Curvature Direction')
@@ -86,5 +86,5 @@ class GenerateForceProfile:
         plt.show()
 
 gen_force_profile = GenerateForceProfile()
-gen_force_profile.getRefinedTrajecotry(example_id=1, noise_level=0.21, k=15)
+gen_force_profile.getRefinedTrajecotry(example_id=1, noise_level=0.21, k=5)
 
